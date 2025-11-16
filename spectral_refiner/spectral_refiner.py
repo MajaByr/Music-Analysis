ef16561dc0c798712a9b4c0899f3df131e66c2a8
MajaByr <majanna.byr@gmail.com>
2025-11-16 22:53:57 +0100

    :sparkles: Add method for filtering out harmonics v1

--- spectral_refiner/spectral_refiner.py
+++ spectral_refiner/spectral_refiner.py
@@ -1,11 +1,13 @@
 import librosa
 import soundfile as sf
 import numpy as np
-from scipy.fft import fft, ifft, fftshift
+# from scipy.fft import fft, ifft, fftshift
+import scipy as sp
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from functools import partial
 from scipy.signal import medfilt
+from scipy.io.wavfile import write
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -19,14 +21,14 @@ class SpectralRefiner:
 
         ixs = np.linspace(0, sampling_rate/2, max_ix, endpoint=False)
 
-        y = fft[1:max_ix]
+        y = fft[:max_ix]
 
         plt.semilogy(ixs, y)
         
         plt.grid()
         plt.show()
 
-    def get_dominant_frequencies(fft: np.ndarray, min_prominence: float = 30):
+    def get_dominant_frequencies_ixs(fft: np.ndarray, min_prominence: float = 30):
         
         as_db = 20 * np.log10(abs(fft) + 1e-12)
         
@@ -35,7 +37,9 @@ class SpectralRefiner:
         
         dominant_frequencies, _ = find_peaks(as_part, prominence=min_prominence)
         dominant_frequencies = [int(df) for df in dominant_frequencies]
-        return dominant_frequencies
+        dom_freq_amplitudes = as_part[dominant_frequencies]
+
+        return dominant_frequencies, dom_freq_amplitudes
     
     def plot_fft_with_dominant_peaks(fft, sampling_rate, dominant_frequencies):
 
@@ -53,36 +57,76 @@ class SpectralRefiner:
         plt.legend()
         plt.grid(True)
         plt.show()
-        
-    def get_conversion_factor(fft, sr) -> int:
-        max = int(len(fft)/2)
-        return 1/max * sr/2
 
-    def ix_to_hz(sample_ix, conversion_factor) -> int:
+    def ix_to_hz(sample_ix, fft, sampling_rate) -> int:
         """
         Convert an FFT bin index to its corresponding frequency in Hertz.
         """
-        return sample_ix * conversion_factor
+        return sample_ix / len(fft) * sampling_rate
+    
+    def hz_to_ix(f_hz, fft, sampling_rate):
+        return round(f_hz / sampling_rate * len(fft))
+    
+    def filter_fft(fft, sampling_rate, dominant_frequencies_ixs, dom_freq_amplitudes, delta = 15):
+        
+        max_ix = len(fft)//2
+
+        freqs = np.linspace(0, sampling_rate/2, max_ix, endpoint=False)
+        dominant_frequencies = freqs[dominant_frequencies_ixs]
+
+        fft_filtered = fft[:max_ix].copy()
+
+        hz_per_bin = sampling_rate / max_ix
+        ix_radius = int(np.ceil(delta / hz_per_bin))
+
+        for idx in dominant_frequencies_ixs:
+            start = max(idx - ix_radius, 0)
+            end = min(idx + ix_radius + 1, max_ix)
+            fft_filtered[start:end] = 0
+            fft_filtered[idx] = fft[idx]
+
+        return fft_filtered
+
+    def fft_to_wav(fft: np.array, wav_path: str, sampling_rate: int):
+        """
+        Save waveform to 16-bit PCM
+        """
+
+        time_signal = sp.fft.ifft(fft)
+        time_signal = np.real(time_signal)
+        
+        # Normalize to <-1, 1>
+        time_signal = time_signal / np.max(np.abs(time_signal))  
+        time_signal_int16 = np.int16(time_signal * 32767)
+
+        write(wav_path, sampling_rate, time_signal_int16)
+        print(f"Saved WAV file: {wav_path}")
+    
+    def analyze_file(wav_path, output_path, min_prominence: float = 30):
+        
+        # Load wav file
+        y, sr = librosa.load(wav_path, sr=None)
+
+        # Calculate FFT
+        fft = sp.fft.fft(y)
+        
+        # Get dominant frequencies
+        dominant_frequencies_ixs, dom_freq_amplitudes = SpectralRefiner.get_dominant_frequencies_ixs(fft, 35)
+
+        # Filter out all harmonics lying within ±15 Hz of the detected spectral peaks
+        filtered_fft = SpectralRefiner.filter_fft(fft, sr, dominant_frequencies_ixs, dom_freq_amplitudes, 15)
+
+        # Save filtered file to .wav
+        SpectralRefiner.fft_to_wav(filtered_fft, output_path, sr)
     
 
 if __name__=="__main__":
     dir_path = "spectral_refiner/audio_samples/"
-    wav_path = dir_path + 'violin_raw.wav'
-
-    # Load wav file
-    y, sr = librosa.load(wav_path, sr=None)
-
-    # Calculate FFT
-    fft = fft(y)
-
-    conversion_factor = SpectralRefiner.get_conversion_factor(fft, sr)
     
-    # Get dominant frequencies
-    dominant_frequencies = SpectralRefiner.get_dominant_frequencies(fft, 35)
+    # # Analyze raw violin
+    # raw_wav_path = dir_path + 'violin_raw.wav'
+    # SpectralRefiner.analyze_file(raw_wav_path, 35)
 
-    # Filter dominant frequencies
-    dominant_frequencies = [df for df in dominant_frequencies if df>100]
-
-    SpectralRefiner.plot_fft_with_dominant_peaks(fft, sr, dominant_frequencies)
-    
-    print(dominant_frequencies)
+    # Analyze violin triad
+    triad_wav_path = dir_path + 'violin_triad.wav'
+    SpectralRefiner.analyze_file(triad_wav_path, 'spectral_refiner/output/violin_triad_filtered.wav', 24)
